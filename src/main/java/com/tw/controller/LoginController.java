69d0a44a2022db11284e70387336ec539cc9c40c
john <john@LAPTOP-8N545TAJ>
2019-08-04 00:11:41 +0800

    1、新增了手机号码校验； 2、将发送短信作为工具类从业务中抽出来； 3、写好了发送短信和校验短信的方法，等待和前端联调； 4、根据短信状态获得是否发送成功的标记，确定是否正确发送验证码；

--- src/main/java/com/tw/controller/LoginController.java
+++ src/main/java/com/tw/controller/LoginController.java
@@ -1,18 +1,18 @@
 package com.tw.controller;
 
-import com.tw.util.DateUtils;
-import com.tw.util.MD5Util;
-import com.tw.util.MathUtil;
-import com.tw.util.SMSUtil;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import com.tw.util.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
+
+import static com.tw.util.ResponseInfo.CODE_ERROR;
+import static com.tw.util.ResponseInfo.CODE_SUCCESS;
 
 /**
  * @Author: John
@@ -26,30 +26,65 @@ public class LoginController {
 
     private static final String KEY = "abc123"; // KEY为自定义秘钥
 
-    @RequestMapping(value = "/sendMsg", method = RequestMethod.POST, headers = "Accept=application/json")
-    public Map<String, Object> sendMsg(@RequestBody Map<String,Object> requestMap) {
-        String[] phoneNumbers = (String[])requestMap.get("phoneNumber");
-        //生成随机数
-        String randomNum = String.valueOf(MathUtil.random(6));
-        SimpleDateFormat sf = new SimpleDateFormat("yyyyMMddHHmmss");
-        Calendar c = Calendar.getInstance();
-        c.add(Calendar.MINUTE, 5);
-        //生成5分钟后时间，用户校验是否过期
-        String currentTime = sf.format(c.getTime());
-        //此处执行发送短信验证码方法
-        SMSUtil.sendOneMessage(phoneNumbers);
-        //生成MD5值
-        String hash =  MD5Util.toMD5(KEY + "@" + currentTime + "@" + randomNum);
-        Map<String, Object> resultMap = new HashMap<>();
-        resultMap.put("hash", hash);
-        resultMap.put("tamp", currentTime);
-        //将hash值和tamp时间返回给前端
-        return resultMap;
+
+    /**
+     * @Author: John
+     * @Description: 发送短信的方法
+     * @Date:  2019/8/4 0:00
+     * @param: requestMap
+     * @return:
+     */
+    @RequestMapping(value = "/smsSendCode", method = RequestMethod.POST, headers = "Accept=application/json")
+    public ResponseInfo sendMsg(@RequestBody Map<String,Object> requestMap) {
+        ResponseInfo response = new ResponseInfo();
+        String phoneNumber = (String) requestMap.get("phoneNumber");
+
+        //校验前端传过来的手机号码是否是正确的，如果正确就继续，否则就返回格式错误
+        Boolean isValidPhoneNumber = PhoneUtil.isNotValidChinesePhone(phoneNumber);
+        if (isValidPhoneNumber) {
+            //生成随机数
+            String randomNum = String.valueOf(MathUtil.random(6));
+            SimpleDateFormat sf = new SimpleDateFormat("yyyyMMddHHmmss");
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.MINUTE, 5);
+            //生成5分钟后时间，用户校验是否过期
+            String currentTime = sf.format(calendar.getTime());
+
+            //此处执行发送短信验证码方法
+            Boolean isSend = SMSUtil.sendOneMessage(phoneNumber);
+            if (isSend) {
+                //生成MD5值
+                String hash =  MD5Util.toMD5(KEY + "@" + currentTime + "@" + randomNum);
+                Map<String, Object> resultMap = new HashMap<>();
+                resultMap.put("hash", hash);
+                resultMap.put("tamp", currentTime);
+                response.setCode(CODE_SUCCESS);
+                response.setMessage("send message success!");
+                //将hash值和tamp时间返回给前端
+                response.setData(resultMap);
+            } else {
+                response.setCode(CODE_ERROR);
+                response.setMessage("send message failed!");
+            }
+        } else {
+            response.setCode(CODE_ERROR);
+            response.setMessage("it's not correct phoneNumber!");
+        }
+        return response;
     }
 
 
-    @RequestMapping(value = "/validateNum", method = RequestMethod.POST, headers = "Accept=application/json")
-    public Map<String, Object> validateNum(@RequestBody Map<String,Object> requestMap) {
+    /**
+     * @Author: John
+     * @Description: 校验短信验证是否正确
+     * @Date:  2019/8/4 0:01
+     * @param: requestMap
+     * @return:
+     */
+    @RequestMapping(value = "/smsValidate", method = RequestMethod.POST, headers = "Accept=application/json")
+    public ResponseInfo validateNum(@RequestBody Map<String,Object> requestMap) {
+        ResponseInfo response = new ResponseInfo();
+        //从前端传过来的数据
         String requestHash = requestMap.get("hash").toString();
         String tamp = requestMap.get("tamp").toString();
         String msgNum = requestMap.get("msgNum").toString();
@@ -57,12 +92,18 @@ public class LoginController {
         if (tamp.compareTo(DateUtils.getDateTime()) > 0) {
             if (hash.equalsIgnoreCase(requestHash)){
                 //校验成功
+                response.setCode(CODE_SUCCESS);
+                response.setMessage("message validate success!");
             }else {
                 //验证码不正确，校验失败
+                response.setCode(CODE_ERROR);
+                response.setMessage("message validate failed!");
             }
         } else {
             // 超时
+            response.setCode(CODE_ERROR);
+            response.setMessage("code is out of time, please send again!");
         }
-        return requestMap;
+        return response;
     }
 }
--- src/main/java/com/tw/util/SMSUtil.java
+++ src/main/java/com/tw/util/SMSUtil.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONException;
 import com.github.qcloudsms.SmsSingleSender;
 import com.github.qcloudsms.SmsSingleSenderResult;
 import com.tw.config.ConfigProperties;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,6 @@ import java.io.IOException;
  */
 public class SMSUtil {
 
-
     @Autowired
     private static ConfigProperties configProperties;
 
@@ -28,7 +28,14 @@ public class SMSUtil {
     private final static Logger logger = LoggerFactory.getLogger(SMSUtil.class);
 
 
-    public static Boolean sendOneMessage(String[] phoneNumbers) {
+    /**
+     * @Author: John
+     * @Description:       发送单条短信
+     * @Date:  2019/8/3 23:12
+     * @param: null
+     * @return:
+     */
+    public static Boolean sendOneMessage(String phoneNumber) {
         //短信验证码
         String RANDOM_INT = MathUtil.randomStr(6);
 
@@ -46,24 +53,39 @@ public class SMSUtil {
         String smsSign = configProperties.getSmsSign();
 
         try {
-            //参数，例如 验证码为5678，3分钟内填写
+            //参数，例如 验证码为5678，5分钟内填写
             String[] params = {RANDOM_INT, smsTime};
             SmsSingleSender ssender = new SmsSingleSender(appid, appkey);
-            logger.info("========参数为：phoneNum:"+phoneNumbers[0]);
-            SmsSingleSenderResult result = ssender.sendWithParam("86",  phoneNumbers[0], templateId, params, smsSign, "", "");
+            logger.info("========参数为：phoneNum:"+phoneNumber);
+            SmsSingleSenderResult result = ssender.sendWithParam("86",  phoneNumber, templateId, params, smsSign, "", "");
             logger.info("========发送短信的结果为:"+result);
+            String resultStr = result.toString();
+            //TODO
+            //如果短信结果是成功的，就需要将短信和手机号码和加密密钥变成MD5码
+            if (StringUtils.isNotEmpty(resultStr)) {
+                if (resultStr.contains("success")) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            return false;
+            //TODO
         } catch (HTTPException e) {
             // HTTP 响应码错误
             e.printStackTrace();
+            return false;
         } catch (JSONException e) {
             // JSON 解析错误
             e.printStackTrace();
+            return false;
         } catch (IOException e) {
             // 网络 IO 错误
             e.printStackTrace();
+            return false;
         } catch (com.github.qcloudsms.httpclient.HTTPException e) {
             e.printStackTrace();
+            return false;
         }
-        return true;
     }
 }
