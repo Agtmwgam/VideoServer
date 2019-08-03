b20815d8cd3d5fb586e1508767ce5b85430e0f2a
sevit112@qq.com <feiyu123>
2019-08-03 12:01:13 +0800

    entity

---
+++ src/main/java/com/tw/entity/vUser.java
@@ -0,0 +1,31 @@
+package com.tw.entity;
+
+import lombok.Data;
+
+@Data
+public class vUser extends BaseEntity {
+    //    用户ID
+    private String userID;
+    //    用户昵称
+    private String name;
+    //    用户账号
+    private String account;
+    //    账户密码
+    private String password;
+    //    手机号
+    private String phone;
+
+    public vUser() {
+        super();
+    }
+
+    public vUser(String userID, String name, String account, String password, String phone) {
+        super();
+        this.userID = userID;
+        this.name = name;
+        this.account = account;
+        this.password = password;
+        this.phone = phone;
+    }
+}
+
