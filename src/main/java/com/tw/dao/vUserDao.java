23bd38182f6fc242e443bae5ff76635c5224e301
sevit112@qq.com <feiyu123>
2019-08-03 14:43:20 +0800

    DAO&Mapper --刘天文

---
+++ src/main/java/com/tw/dao/vUserDao.java
@@ -0,0 +1,14 @@
+package com.tw.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author liutianwen
+ * @Description:
+ * @date 2019年8月3日
+ */
+@Mapper
+public interface vUserDao {
+	
+
+}
