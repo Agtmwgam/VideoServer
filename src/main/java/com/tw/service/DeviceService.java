341d9a397d2553d3c8aaa3bda3079a6427151b2f
john <john@LAPTOP-8N545TAJ>
2019-08-05 21:48:30 +0800

    1、去掉多余的代码；

--- src/main/java/com/tw/service/DeviceService.java
+++ src/main/java/com/tw/service/DeviceService.java
@@ -5,9 +5,6 @@ import com.tw.entity.Device;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Service
 public class DeviceService {
 
@@ -15,12 +12,6 @@ public class DeviceService {
     private DeviceDao deviceDao;
 
     public int addDevice(Device device) {
-        Map<String, Object> param =  new HashMap<>();
-        param.put("serial", device.getSerial());
-        param.put("deviceVerifyCode", device.getDeviceVerifyCode());
-        param.put("deviceType", device.getDeviceType());
-        param.put("softVersion", device.getSoftVersion());
-        param.put("productDate", device.getProductDate());
         return deviceDao.addDevice(device);
     }
 }
