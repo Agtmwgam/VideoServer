bc2c44a66fee641c98d14732adf784edb6dc0c77
zhuoshouyi <928727991@qq.com>
2019-08-06 09:45:28 +0800

    增加 vUser2UserRoleDTOConvert 转换类。

---
+++ src/main/java/com/tw/convert/vUser2UserRoleDTOConvert.java
@@ -0,0 +1,22 @@
+package com.tw.convert;
+
+import com.tw.dto.UserRoleDTO;
+import com.tw.entity.vUser;
+
+/**
+ * 功能: 将 vUser 类转换为 UserRoleDTO 类.
+ *
+ * Created by zhuoshouyi on 2019/8/5.
+ */
+public class vUser2UserRoleDTOConvert {
+
+    public static UserRoleDTO convert(vUser user){
+
+        UserRoleDTO userRoleDTO = new UserRoleDTO();
+        userRoleDTO.setUserID(user.getUserID());
+        userRoleDTO.setNickName(user.getNickName());
+        userRoleDTO.setPhoneNumber(user.getPhoneNumber());
+
+        return userRoleDTO;
+    }
+}
