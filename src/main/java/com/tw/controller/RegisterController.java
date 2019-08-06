5f22f1145b0405afb3bb085ad447a77e3cf1a09a
sevit112@qq.com <feiyu123>
2019-08-06 23:33:15 +0800

    修改用户注册类

--- src/main/java/com/tw/controller/RegisterController.java
+++ src/main/java/com/tw/controller/RegisterController.java
@@ -5,6 +5,7 @@ import com.tw.entity.common.ConstantParam;
 import com.tw.service.MessageService;
 import com.tw.service.VUserService;
 import com.tw.util.ResponseInfo;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,7 +36,32 @@ public class RegisterController {
     public ResponseInfo createUser(VUser user, String verifyCode) {
         user.setPhoneNumber("18210081211");
 
+//       检查用户注册信息(手机号，密码，验证码)是否正常
+        ResponseInfo response = checkUserInfo(user, verifyCode);
+        if (response.getCode() == "9999") {
+            return response;
+        }
+
+        //创建用户
+        Boolean flag = userService.creatUser(user);
+        response.setCode("0000");
+        response.setMessage(" Registered successfully!");
+
+        return response;
+    }
+
+    /**
+     * @return
+     * @Date 2019/8/5 22:21
+     * @Created liutianwen
+     * @Description 校验用户注册时，所有参数是否正常
+     */
+    public ResponseInfo checkUserInfo(VUser user, String verifyCode) {
         ResponseInfo response = new ResponseInfo();
+        //       用户注册手机号
+        String phoneNumber = user.getPhoneNumber();
+//       用户注册密码
+        String password = user.getPassword();
         //手机号码是否正规范
         Boolean isRightPhone = false;
         //验证码是否正确
@@ -43,16 +69,29 @@ public class RegisterController {
         //密码是否规范
         Boolean isRightPassword = false;
 
+        if (StringUtils.isBlank(phoneNumber)) {
+            response.setCode("9999");
+            response.setMessage("The phoneNumber can not be empty!");
+            return response;
+        }
+        if (StringUtils.isBlank(password)) {
+            response.setCode("9999");
+            response.setMessage("The password can not be empty!");
+            return response;
+        }
+        if (StringUtils.isBlank(verifyCode)) {
+            response.setCode("9999");
+            response.setMessage("The verifyCode can not be empty!");
+            return response;
+        }
+
 
         //手机号码校验是否规范
-        isRightPhone = user.getPhoneNumber().matches(ConstantParam.VERIFYPHONENUMBER);
-
+        isRightPhone = phoneNumber.matches(ConstantParam.VERIFYPHONENUMBER);
 //        TODO 调用短信验证码验证接口
-//        Boolean isRightCode=messageService.sendMessage(user.getPhoneNumber());
-
+//       isRightCode=messageService.sendMessage(user.getPhoneNumber());
         //密码是否符合规范
-        isRightPassword = user.getPassword().matches(ConstantParam.VERIFYPASSWORD);
-
+        isRightPassword = password.matches(ConstantParam.VERIFYPASSWORD);
         //手机号码是否规范
         if (!isRightPhone) {
             response.setCode("9999");
@@ -73,16 +112,9 @@ public class RegisterController {
             response.setMessage("The password is not correct!");
             return response;
         }
-
-
-        //创建用户
-        Boolean flag = userService.creatUser(user);
-        response.setCode("0000");
-        response.setMessage(" Registered successfully!");
-
+        response.setCode("9999");
         return response;
     }
-
 
     public static void main(String[] args) {
         RegisterController rc = new RegisterController();
