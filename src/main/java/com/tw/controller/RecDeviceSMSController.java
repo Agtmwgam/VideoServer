0e20a205a7e2eb87bd3b654d99787034d7281653
zhuoshouyi <928727991@qq.com>
2019-08-08 23:36:54 +0800

    增加entity继承

--- src/main/java/com/tw/controller/RecDeviceSMSController.java
+++ src/main/java/com/tw/controller/RecDeviceSMSController.java
@@ -3,11 +3,11 @@ package com.tw.controller;
 import com.tw.service.RecDeviceSMSService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/recMessage")
 @Slf4j
 public class RecDeviceSMSController {
